3609b7b1f90bfe41b65655ae5f7b51db5d9e50db
kushar77 <kusharora339@gmail.com>
2024-01-11 12:54:50 -0800

    added start/stop functionality

--- App.tsx
+++ App.tsx
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StopWatchButton from './src/StopWatchButton';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import StopWatch from './src/StopWatch';
 
 export default function App() {
 
   const [isRunning, setIsRunning] = useState(false);
+  const [timePassed, setTimePassed] = useState(0);
+  const timeRef = useRef<number | null>(null);
 
   const handleRun = () => {
     setIsRunning(!isRunning);
   }
 
+  useEffect(() => {
+    if (isRunning) { // start watch
+      const prevTime = new Date(Date.now() - timePassed);
+      timeRef.current = setInterval(() => {
+        setTimePassed(Date.now() - prevTime);
+      }, 1000); // updating every second
+    } else { // stop watch
+      clearInterval(timeRef.current as number);
+    }
+
+    return () => {
+      clearInterval(timeRef.current as number);
+    }
+  }, [isRunning]);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <StopWatch time={1000} />
+      <StopWatch time={timePassed} />
       <StopWatchButton isRunning={isRunning} handleRun={handleRun} />
     </View>
   );
