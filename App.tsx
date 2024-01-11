a8a9d48d3a91a7548e235dd142e9a560ad8e069d
kushar77 <kusharora339@gmail.com>
2024-01-11 12:09:58 -0800

    added start, reset, lap buttons

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StopWatchButton from './src/StopWatchButton';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [isRunning, setIsRunning] = useState(false);
+
+  const handleRun = () => {
+    setIsRunning(!isRunning);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <StopWatchButton isRunning={isRunning} handleRun={handleRun} />
     </View>
   );
 }
@@ -13,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,8 +1,51 @@
-import { View } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import React from 'react';
 
-export default function StopWatchButton() {
+interface ButtonProps {
+  isRunning: boolean;
+  handleRun: () => void;
+}
+
+const StopWatchButton: React.FC<ButtonProps> = (props) => {
   return (
-    <View >
+    <View style={styles.buttonContainer}>
+      <TouchableOpacity 
+        style={
+          [styles.button, props.isRunning ? {backgroundColor: "#ff0000"} : {backgroundColor: "#7cfc00"}]
+        }
+        onPress={props.handleRun}
+      >
+        <Text>{props.isRunning ? "Stop" : "Start"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, {backgroundColor: "#00bfff"}]}
+      >
+        <Text>Reset</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, {backgroundColor: "#ffa07a"}]}
+      >
+        <Text>Lap</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 15,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center', 
+    justifyContent: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginHorizontal: 5
+  }
+});
+
+export default StopWatchButton;
