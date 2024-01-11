472977c9554f271c72141e3bb2fac004344f1899
Evan Pelletier <av8@escapeware.ca>
2024-01-11 14:22:04 -0500

    Fixed file hierarchy in JS file

--- tests/Stopwatch.test.js
+++ tests/Stopwatch.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
-import Stopwatch from '../src/Stopwatch';
+import Stopwatch from './src/Stopwatch';
 
 describe('Stopwatch', () => {
   test('renders initial state correctly', () => {
