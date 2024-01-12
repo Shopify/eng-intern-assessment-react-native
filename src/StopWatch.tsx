c11347f4fda734e05a3f5dc8261b5f9972a024b4
Kishor Pandya <kishorpandya17@gmail.com>
2024-01-12 17:37:45 -0500

    added the lap functionality

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -7,6 +7,7 @@ export default function StopWatch() {
 
   const [isOn, setIsOn] = useState(false);
   const [time, setTime] = useState(0);
+  const [laps, setLaps] = useState<{[key: number]: number}>({});
 
   useEffect(() => {
     let timeInterval: number | undefined;
@@ -33,13 +34,32 @@ export default function StopWatch() {
   const reset = () => {
     setIsOn(false);
     setTime(0);
-  }
+    setLaps({});
+  };
+
+  const lap = () => {
+    if(isOn) {
+      setLaps((previousLaps) => ({
+        ...previousLaps,
+        [`lap${Object.keys(previousLaps).length + 1}`]: time,
+      }))
+    }
+  };
 
   return (
     <View >
       <Text>{displayTime(time)}</Text>
       <StopWatchButton title={isOn ? 'Stop' : 'Start'} onPress={startStop}/>
       <StopWatchButton title='Reset' onPress={reset}/>
+      <StopWatchButton title='Lap' onPress={lap} />
+      {Object.entries(laps).length > 0 && (
+        <View>
+          <Text>Lap Times:</Text>
+          {Object.entries(laps).map(([lapKey, lapTime]) => (
+            <Text key={lapKey}>{`${lapKey}: ${displayTime(lapTime)}`}</Text>
+          ))}
+        </View>
+      )}
     </View>
   );
 }
