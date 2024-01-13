14c57dcb01725c92d7489f8b147b8522def6c6fc
lancenigel <lancenigelmadarang@gmail.com>
2024-01-13 14:29:10 -0500

    Stopwatch Implementation Lance Madarang Jan 12 2024

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import { Text, View, StyleSheet} from 'react-native';
 import StopWatchButton from './StopWatchButton';
 
 
