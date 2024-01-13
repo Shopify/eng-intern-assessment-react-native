b154186f49ecc9f2ca5a590d00342f05dc279149
Nina Huang <nina.hhhuang@gmail.com>
2024-01-13 09:41:04 -0500

    Implement StopWatchButton

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,8 +1,32 @@
-import { View } from 'react-native';
+import React from 'react';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-export default function StopWatchButton() {
+export default function StopWatchButton(props) {
+  const { onClick, color, title } = props;
   return (
-    <View >
-    </View>
+    <TouchableOpacity
+    onPress={onClick}
+    style={[styles.button, {backgroundColor: color}]}
+    activeOpacity={0.6}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 95,
+    height: 40,
+    borderRadius: 30,
+    marginTop: '5%'
+  },
+  text: {
+    fontSize: 16,
+    fontFamily: 'Courier New',
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+})
