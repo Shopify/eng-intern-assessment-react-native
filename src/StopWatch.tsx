bc722a066aa08b1fefe9745da080580b20715977
lancenigel <lancenigelmadarang@gmail.com>
2024-01-13 14:43:23 -0500

    cleaned up code

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,107 +1,96 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import StopWatchButton from './StopWatchButton';
 
-
-
 const StopWatch = () => {
+  // State variables
   const [isRunning, setIsRunning] = useState(false);
   const [timeElapsed, setTimeElapsed] = useState(0);
   const [laps, setLaps] = useState<number[]>([]);
-  const [isNightMode, setIsNightMode] = useState(false); // Add night mode state
+  const [isNightMode, setIsNightMode] = useState(false);
 
+  // Interval handling for timeElapsed
   useEffect(() => {
     let interval: ReturnType<typeof setInterval>;
+
     if (isRunning) {
       interval = setInterval(() => {
         setTimeElapsed(prevTime => prevTime + 10); // Increment every 10 milliseconds
       }, 10);
     }
+
     return () => clearInterval(interval);
   }, [isRunning]);
 
+  // Handle starting/stopping the stopwatch
   const handleStartStop = () => {
     setIsRunning(!isRunning);
   };
 
   let lapStartTime = 0;
-
+  // Handle adding a lap
   const handleLap = () => {
     if (isRunning) {
-      // Calculate lap time based on the difference between current time and lap start time
       const lapTime = timeElapsed - lapStartTime;
-
-      // Update lap start time for the next lap
       lapStartTime = timeElapsed;
-
-      // Add the new lap
       const newLaps = [...laps, lapTime];
 
-      // Check if the length exceeds 10
       if (newLaps.length > 10) {
-        // Remove the oldest lap
-        newLaps.shift();
+        newLaps.shift(); // Remove the oldest lap
       }
 
-      // Update the state with the modified laps array
       setLaps(newLaps);
     }
   };
 
+  // Handle resetting the stopwatch
   const handleReset = () => {
     setIsRunning(false);
     setTimeElapsed(0);
     setLaps([]);
   };
 
+  // Handle toggling night mode
   const toggleNightMode = () => {
     setIsNightMode(!isNightMode);
   };
 
-  const formatTimeUnit = (unit: number) => {
-    // Helper function to ensure time units are always displayed with two digits
-    return unit < 10 ? `0${unit}` : unit.toString();
-  };
+  // Format time units
+  const formatTimeUnit = (unit: number) => (unit < 10 ? `0${unit}` : unit.toString());
 
+  // Format the time
   const formatTime = (milliseconds: number) => {
-    const hours = Math.floor(milliseconds / 3600000); // 1 Hour = 3600000 Milliseconds
-    const minutes = Math.floor((milliseconds % 3600000) / 60000); // 1 Minute = 60000 Milliseconds
-    const seconds = Math.floor((milliseconds % 60000) / 1000); // 1 Second = 1000 Milliseconds
-    const millis = Math.floor((milliseconds % 1000) / 10); // Display only two digits for milliseconds
-  
+    const hours = Math.floor(milliseconds / 3600000);
+    const minutes = Math.floor((milliseconds % 3600000) / 60000);
+    const seconds = Math.floor((milliseconds % 60000) / 1000);
+    const millis = Math.floor((milliseconds % 1000) / 10);
+
     return `${formatTimeUnit(hours)}:${formatTimeUnit(minutes)}:${formatTimeUnit(seconds)}:${formatTimeUnit(millis)}`;
   };
-  
-  const dayModeStyles = {
-    container: {
-      // Day mode container styles
-    },
-    timerText: {
-      // Day mode timer text styles
-    },
-    lapText: {
 
-    }
+  // Styles
+  const dayModeStyles = {
+    container: {},
+    timerText: {},
+    lapText: {},
     // ... Other day mode styles
   };
 
   const nightModeStyles = {
     container: {
-      // Night mode container styles
-      backgroundColor: 'black', // Example background color for night mode
+      backgroundColor: 'black',
     },
     timerText: {
-      // Night mode timer text styles
-      color: 'white', // Example text color for night mode
+      color: 'white',
     },
     lapText: {
       color: 'white',
-    }
+    },
     // ... Other night mode styles
   };
 
   const currentStyles = isNightMode ? nightModeStyles : dayModeStyles;
-  
+
   return (
     <View style={{ ...styles.outerContainer, ...currentStyles.container }}>
       <View style={{ ...styles.container, ...currentStyles.container }}>
@@ -111,45 +100,41 @@ const StopWatch = () => {
       </View>
 
       <View style={styles.buttonAndLapsContainer}>
-      <StopWatchButton title={`Switch to ${isNightMode ? 'Day' : 'Night'} Mode`} onPress={toggleNightMode}/>
-
+        <StopWatchButton title={`Switch to ${isNightMode ? 'Day' : 'Night'} Mode`} onPress={toggleNightMode} />
 
         <View style={styles.buttonContainer}>
-          <StopWatchButton title={isRunning ? "Stop" : "Start"} onPress={handleStartStop} />
+          <StopWatchButton title={isRunning ? 'Stop' : 'Start'} onPress={handleStartStop} />
           <StopWatchButton title="Lap" onPress={handleLap} />
           <StopWatchButton title="Reset" onPress={handleReset} />
         </View>
 
         <View style={styles.lapsContainer}>
-        {laps.slice().reverse().map((lap, index) => (
-          <Text key={index} style={{ ...styles.lapText, ...currentStyles.lapText }}>
-            Lap {laps.length - index}: {formatTime(lap)}
-          </Text>
-        ))}
+          {laps.slice().reverse().map((lap, index) => (
+            <Text key={index} style={{ ...styles.lapText, ...currentStyles.lapText }}>
+              Lap {laps.length - index}: {formatTime(lap)}
+            </Text>
+          ))}
+        </View>
       </View>
-      </View>
-
-      
-
     </View>
   );
 };
 
+// Styles
 const styles = StyleSheet.create({
   outerContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    width: '100%', // Full width to center the inner container
+    width: '100%',
   },
   container: {
-    // Instead of minWidth, we'll use width here to ensure the container size is fixed.
-    width: 350, // This width should be the same or greater than the widest possible time string.
-    alignItems: 'flex-start', // This will center the text horizontally within the container.
-    position: "absolute",
-    top: "30%",
-    left: "18%",
-    justifyContent: 'center', // This will center the text vertically within the container.
+    width: 350,
+    alignItems: 'flex-start',
+    position: 'absolute',
+    top: '30%',
+    left: '18%',
+    justifyContent: 'center',
   },
   timerText: {
     fontSize: 40,
@@ -160,30 +145,24 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   buttonContainer: {
-    flexDirection: 'row', // This will arrange buttons side by side
+    flexDirection: 'row',
     justifyContent: 'space-around',
-    width: '50%', // Adjust as needed
+    width: '50%',
     zIndex: 1,
     marginTop: 20,
   },
   lapsContainer: {
-    marginTop: 30, // Adjust the margin as needed
+    marginTop: 30,
     position: 'absolute',
-    top: '80%', // Place laps below buttons
-    width: '100%', // Use the full width of the screen for the laps container
-    alignItems: 'center', // Center-align the lap text
+    top: '80%',
+    width: '100%',
+    alignItems: 'center',
   },
   buttonAndLapsContainer: {
-    flexDirection: 'column', // Stack children vertically
-    alignItems: 'center', // Center-align children horizontally
-    marginTop: 20,
-  },  
-  nightModeButton: {
+    flexDirection: 'column',
     alignItems: 'center',
-    marginTop: -20,
-    marginLeft: 20,
+    marginTop: 20,
   },
 });
 
 export default StopWatch;
-
