0006e38302c6f4c357a5bb70a34d679807451d1b
meahb <meahbachert@gmail.com>
2024-01-16 12:58:45 -0500

    chore: create and set up colors.ts file

---
+++ config/colors.ts
@@ -0,0 +1,9 @@
+export default {
+    primaryBackground: '#22223a',
+    secondaryBackground: '#2c2c4b',
+    white: 'white',
+    accentColor1: '#37595e',
+    accentColor2: '#00e5e3',
+    accentColor3: '#2aa4a2',
+    accentColor4: '#3b8483',
+};
