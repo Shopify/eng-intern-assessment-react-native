e3cb0769a85699e707c7ab46f9130d8846e533aa
Kamel <kamel.laurentd@gmail.com>
2024-01-18 21:31:14 -0500

    cleanup

--- App.tsx
+++ App.tsx
@@ -3,8 +3,6 @@ import { StyleSheet, View } from "react-native";
 import MainView from "./src/MainView";
 
 export default function App() {
-  const [time, setTime] = React.useState(0);
-  const [laps, setLaps] = React.useState([]);
   return (
     <View style={styles.container}>
       <MainView></MainView>
