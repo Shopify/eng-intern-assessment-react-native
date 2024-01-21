c644a03df649b72e38474bff2bd3fa2b98d7c2af
leonapples <58273463+leonapples@users.noreply.github.com>
2024-01-21 17:06:25 -0500

    feat: basic functionality and appearance of StopWatch component

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,30 @@
-import { View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { StopWatchProps } from './types';
 
-export default function StopWatch() {
+export default function StopWatch(props: StopWatchProps) {
+  const {
+    formattedTime = '00:00:00'
+  } = props;
+
   return (
-    <View >
+    <View style={styles.display}>
+      <Text style={styles.text}> {formattedTime} </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  display: {
+    width: 300,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+    backgroundColor: "#ADD8E6",
+    marginVertical: 20,
+  },
+  text: {
+    fontSize: 30,
+    color: "#5A5A5A"
+  }
+});
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,7 @@ export interface StopWatchButtonProps {
   text: string;
   onPress: (() => void) | undefined;
 }
+
+export interface StopWatchProps {
+  formattedTime: string;
+}
