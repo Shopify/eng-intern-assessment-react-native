07ddc583ea92a526a3fee3857e5665b5f3ab0cce
Robert336 <rmazza33@gmail.com>
2024-01-21 17:14:01 -0500

    feat: add a title

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { ThemeProvider } from '@shopify/restyle';
 import StopWatch from './src/StopWatch';
 import theme from './src/theme';
 import Box from './src/Box';
+import Text from './src/Text';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <Box backgroundColor='mainBackground' width='100%' height='100%'>
         <SafeAreaView>
+          <Text textAlign='center' padding='m'>StopWatchify</Text>
           <StopWatch/>
           <StatusBar style="auto" />
         </SafeAreaView>
