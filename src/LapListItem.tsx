af43541211a43483781239997a7e2ccd8e2b8fbd
Robert336 <rmazza33@gmail.com>
2024-01-21 17:21:24 -0500

    feat: add drop shadow to buttons

--- src/LapListItem.tsx
+++ src/LapListItem.tsx
@@ -15,6 +15,10 @@ export default function LapListItem ({laptime, lapNum}: LapListItemProps) {
             flex={1}
             flexDirection='row'
             justifyContent='space-between'
+            paddingVertical='s'
+            paddingHorizontal='xl'
+            borderTopWidth={2}
+            borderColor='listBorder'
             >
             <Text>Lap {lapNum}</Text>
             <Text>{formatTimeStr(laptime)}</Text>
