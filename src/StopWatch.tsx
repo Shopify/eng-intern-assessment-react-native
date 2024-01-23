feb3b6de86c70105e6a302e3eddb588c1a8e3ec8
maxbueckert <maxbueckert@gmail.com>
2024-01-23 11:25:10 -0800

    test push

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,6 @@
-import { View } from 'react-native';
+import { View } from "react-native";
+// Test
 
 export default function StopWatch() {
-  return (
-    <View >
-    </View>
-  );
+  return <View></View>;
 }
