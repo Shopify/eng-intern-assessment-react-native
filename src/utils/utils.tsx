30f4c7358b881eafe7c370d2054afa3cde5d07f4
Vincent Zhu <vincenthero16@gmail.com>
2024-01-25 01:37:46 -0500

    Change time format

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,9 +1,12 @@
-export const formatNumber = (num: number): string => num.toString().padStart(2, '0');
+export const formatNumber = (num: number): string =>
+  num.toString().padStart(2, "0");
 
 export const formatTime = (time: number): string => {
   const milliseconds = Math.floor((time % 1000) / 10);
   const seconds = Math.floor((time / 1000) % 60);
   const minutes = Math.floor((time / (1000 * 60)) % 60);
 
-  return `${formatNumber(minutes)}:${formatNumber(seconds)}.${formatNumber(milliseconds)}`;
+  return `${formatNumber(minutes)}:${formatNumber(seconds)}:${formatNumber(
+    milliseconds
+  )}`;
 };
