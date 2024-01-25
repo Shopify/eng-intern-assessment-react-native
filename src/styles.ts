97e380ca65099a13030ef7d96911291bbc044d39
Vincent Zhu <vincenthero16@gmail.com>
2024-01-25 01:32:07 -0500

    Basic StopWatch Done

---
+++ src/styles.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  black: "#000",
+  gray: "#333",
+  white: "#FFF",
+  green: '#092911',
+  lightGreen: '#2ED259',
+  red:  '#340E0B',
+  lightRed: '#FF4539',
+};
+
+export const fonts = {
+  main: 'monospace',
+};
