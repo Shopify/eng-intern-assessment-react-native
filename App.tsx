5d03dc4dfaf955150ae282bf18eb043d8497afad
Asadullah Mohammed <asadullahmohammed@Asadullahs-MacBook-Air.local>
2024-01-25 20:52:18 -0500

    StopWatch

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StopWatch from './src/StopWatch';
+import StopWatchButton from './src/StopWatchButton';
 
 export default function App() {
   return (
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,31 @@
-import { View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function StopWatch() {
+interface StopwatchProps {
+  elapsedTime: number;
+}
+
+const Stopwatch: React.FC<StopwatchProps> = ({ elapsedTime }) => {
   return (
-    <View >
+    <View style={styles.stopwatchContainer}>
+      <Text style={styles.stopwatchText}>{formatTime(elapsedTime)}</Text>
     </View>
   );
-}
+};
+
+const formatTime = (time: number): string => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+};
+
+const styles = StyleSheet.create({
+  stopwatchContainer: {
+    marginBottom: 20,
+  },
+  stopwatchText: {
+    fontSize: 36,
+  },
+});
+
+export default Stopwatch;
