1d890bcc071c8850032ba86acece28ec5fe7f487
Asadullah Mohammed <asadullahmohammed@Asadullahs-MacBook-Air.local>
2024-01-25 20:54:31 -0500

    StopWatch comments

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 interface StopwatchProps {
-  elapsedTime: number;
+  elapsedTime: number; // Props to receive the elapsed time from the parent component
 }
 
+// Stopwatch component to display the elapsed time
 const Stopwatch: React.FC<StopwatchProps> = ({ elapsedTime }) => {
   return (
     <View style={styles.stopwatchContainer}>
@@ -13,19 +14,24 @@ const Stopwatch: React.FC<StopwatchProps> = ({ elapsedTime }) => {
   );
 };
 
+// Function to format the time as MM:SS
 const formatTime = (time: number): string => {
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
   return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 };
 
+
+// Styles for the Stopwatch component
 const styles = StyleSheet.create({
   stopwatchContainer: {
     marginBottom: 20,
-  },
+  },    // Styles for the Stopwatch container
+
+  
   stopwatchText: {
     fontSize: 36,
-  },
+  },    // Styles for the Stopwatch Text 
 });
 
 export default Stopwatch;
