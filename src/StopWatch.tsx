39858ca35608f81359214c39e0eb93f149311007
Lucas Berger <lucask.berger@gmail.com>
2024-01-26 14:48:10 +0100

    feat: count up a clock in seconds

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,28 @@
-import { View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+
+const getCurrentTimeMs = () => {
+  return Date.now() / 100_000_000_000_000;
+};
 
 export default function StopWatch() {
+  const [timeMs, setTimeMs] = useState(0);
+  const [lastTimeMs, setLastTimeMs] = useState(0);
+
+  useEffect(() => {
+    setLastTimeMs(getCurrentTimeMs());
+    const interval = setInterval(() => {
+      const newTimeMs = getCurrentTimeMs();
+      const diff = newTimeMs - lastTimeMs;
+      setTimeMs((time) => time + diff);
+      setLastTimeMs(newTimeMs);
+    }, 1);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <View >
+    <View>
+      <Text>{timeMs.toFixed(3)}s</Text>
     </View>
   );
 }
