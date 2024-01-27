ad52db37fa15924327bb732785d4b9574ed9362b
DevPelumi <oyelowopelumi@gmail.com>
2024-01-27 17:14:32 -0330

    fix: optimized code to pass tests

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -6,51 +6,48 @@ interface StopwatchProps {}
 
 const Stopwatch: React.FC<StopwatchProps> = () => {
   const [time, setTime] = useState<number>(0);
+  const [isTimerActive, setIsTimerActive] = useState<boolean>(true);
   const [running, setRunning] = useState<boolean>(false);
   const [lapTimes, setLapTimes] = useState<number[]>([]);
   
-  const timerRef = useRef<number | null>(null);
+  const timer = useRef<number | null>(null);
 
-  const startTimer = () => {
-    timerRef.current = setInterval(() => {
-      setTime((prevTime) => prevTime + 1);
-    }, 1000);
-  };
-
-  const stopTimer = () => {
-    if (timerRef.current) {
-      clearInterval(timerRef.current);
-    }
-  };
-
-  const toggleTimer = () => {
+  useEffect(() => {
     if (running) {
-      stopTimer();
-    } else {
-      startTimer();
+      timer.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
     }
-    setRunning(!running);
-  };
+    
+    return () => {
+      if (timer.current) {
+        clearInterval(timer.current);
+      }
+    };
+  }, [running]);
 
-  const resetTimer = () => {
-    stopTimer();
-    setTime(0);
-    setLapTimes([]);
-    setRunning(false);
+  const format = (time: number): string => {
+    let hours: string | number = Math.floor(time / 3600);
+    let minutes: string | number = Math.floor((time % 3600) / 60);
+    let seconds: string | number = Math.floor(time % 60);
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    return `${hours}:${minutes}:${seconds}`;
   };
 
   const recordLap = () => {
     setLapTimes((prevLapTimes) => [time, ...prevLapTimes]);
   };
 
-  const formatTime = (time: number): string => {
-    const padZero = (num: number) => (num < 10 ? '0' + num : num.toString());
-
-    const hours = padZero(Math.floor(time / 3600));
-    const minutes = padZero(Math.floor((time % 3600) / 60));
-    const seconds = padZero(Math.floor(time % 60));
-
-    return `${hours}:${minutes}:${seconds}`;
+  const stopTimer = () => {
+    if (timer.current) {
+      clearInterval(timer.current);
+    }
+    setRunning(false);
+    setTime(0)
   };
 
   const styles = StyleSheet.create({
@@ -58,40 +55,64 @@ const Stopwatch: React.FC<StopwatchProps> = () => {
       flex: 1,
       paddingTop: 200,
     },
-    timer: {
+    item: {
+      padding: 10,
+      fontSize: 18,
+      height: 44,
+    },
+    list:{
+      height:100
+    },
+    timer:{
       textAlign: 'center',
       fontSize: 64,
       fontWeight: '500',
-    },
-  });
-
-  useEffect(() => {
-    if (running) {
-      startTimer();
     }
-
-    return stopTimer;
-  }, [running]);
-
+  });
+  
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.timer}>{formatTime(time)}</Text>
+      {isTimerActive && <Text style={styles.timer}>{format(time)}</Text>}
       </View>
       <View>
         <StopwatchButton label="Lap" onPress={recordLap} />
-        <StopwatchButton label={running ? "Stop" : "Start"} onPress={toggleTimer} />
-        <StopwatchButton label={running ? "Pause" : "Resume"} onPress={toggleTimer} />
-        <StopwatchButton label="Reset" onPress={resetTimer} />
+        <StopwatchButton
+          label={running ? "Stop" : "Start"}
+          onPress={() => {
+            if (running) {
+              stopTimer()
+              setIsTimerActive(false)
+            }
+            else {
+              setRunning(true)
+              setIsTimerActive(true)
+            };
+          }}
+        />
+        <StopwatchButton
+          label={running ? "Pause" : "Resume"}
+          onPress={() => {
+            setRunning(!running);
+          }}
+        />
+        <StopwatchButton
+          label='Reset'
+          onPress={() => {
+            stopTimer();
+            setTime(0);
+            setLapTimes([]);
+          }}
+        />
       </View>
       <ScrollView>
-        {lapTimes.length > 0 && (
-          <View testID="lap-list">
-            {lapTimes.map((item, index) => (
-              <Text key={index}>{formatTime(item)}</Text>
+        {lapTimes.length ? (
+          <View testID='lap-list'>
+            {lapTimes.map((item) => (
+              <Text key={item}>{format(item)}</Text>
             ))}
           </View>
-        )}
+        ) : null}
       </ScrollView>
     </View>
   );
