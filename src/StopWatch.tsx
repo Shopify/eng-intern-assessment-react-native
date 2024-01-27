4eb37a71ea09731a48c1484f57380bc46709a623
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2024-01-27 18:57:36 -0500

    Add buttons to StopWatch

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,31 @@
 import { View } from 'react-native';
+import StopWatchButton from './StopWatchButton';
 
 export default function StopWatch() {
+  const handlePressStart = () => {
+    console.log("Start");
+  };
+
+  const handlePressStop = () => {
+    console.log("Stop");
+  };
+
+  const handlePressReset = () => {
+    console.log("Reset");
+  };
+
+  const handlePressLap = () => {
+    console.log("Lap");
+  };
+
   return (
-    <View >
-    </View>
+    <>
+      <View>
+        <StopWatchButton btnTitle='Start' onPressButton={handlePressStart}/>
+        <StopWatchButton btnTitle='Stop' onPressButton={handlePressStop}/>
+        <StopWatchButton btnTitle='Reset' onPressButton={handlePressReset}/>
+        <StopWatchButton btnTitle='Lap' onPressButton={handlePressLap}/>
+      </View>
+    </>
   );
 }
