27437f335ff97d1a419c0c496fcae2cb381a52ee
Thisisme-Andrew <thisemailstotroll@gmail.com>
2024-01-29 04:00:48 +0800

    Stopwatch and App.tsx configured

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,13 @@
+import React from 'react';
 import { View } from 'react-native';
+import StopWatchButton from './StopWatchButton';
 
 export default function StopWatch() {
   return (
     <View >
+      <StopWatchButton title="start" onPress={()=>{console.log("something")}}/>
+      <StopWatchButton title="stop" onPress={()=>{console.log("something1")}}/>
+      <StopWatchButton title="lap" onPress={()=>{console.log("something2")}}/>
     </View>
   );
 }
