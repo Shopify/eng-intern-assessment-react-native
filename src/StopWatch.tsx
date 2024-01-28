3228d1025b0732d34aae12c01de74d8939512bcf
Yazan Armoush <yazan.armoush1@gmail.com>
2024-01-28 07:04:34 -0500

    Fixed Lap Timing Error

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -5,20 +5,36 @@ import StopWatchLapTable from './StopWatchLapTable';
 
 export default function StopWatch() {
   const [isRunning, setIsRunning] = useState<Boolean>(false);
+
   const [startTime, setStartTime] = useState(0);
   const [elapsedTime, setElapsedTime] = useState(0);
   const intervalRef = useRef<number | null>(null);
+
+  const [startLapTime, setStartLapTime] = useState(0);
+  const [elapsedLapTime, setElapsedLapTime] = useState(0);
+  const lapIntervalRef = useRef<number | null>(null);
   const [lapList, setLapList] = useState<string[]>([]);
 
   const startStopwatch = () => {
     const now = Date.now() - elapsedTime;
+    const lapNow = Date.now() - elapsedLapTime;
+
     setStartTime(now);
+    setStartLapTime(lapNow);
     setIsRunning(true);
+
     if (intervalRef.current !== null) {
       clearInterval(intervalRef.current);
     }
+    if (lapIntervalRef.current !== null) {
+      clearInterval(lapIntervalRef.current);
+    }
+    
     intervalRef.current = setInterval(() => {
-        setElapsedTime(Date.now() - now);
+      setElapsedTime(Date.now() - now);
+    }, 100);
+    lapIntervalRef.current = setInterval(() => {
+      setElapsedLapTime(Date.now() - lapNow);
     }, 100);
   };
 
@@ -26,6 +42,9 @@ export default function StopWatch() {
     setIsRunning(false);
     if (intervalRef.current !== null) {
       clearInterval(intervalRef.current);
+    }
+    if (lapIntervalRef.current !== null) {
+      clearInterval(lapIntervalRef.current);
     }
   };
 
@@ -35,13 +54,36 @@ export default function StopWatch() {
       clearInterval(intervalRef.current);
       intervalRef.current = null;
     }
+    if (lapIntervalRef.current !== null) {
+      clearInterval(lapIntervalRef.current);
+      lapIntervalRef.current = null;
+    }
     setStartTime(0);
     setElapsedTime(0);
+
+    setStartLapTime(0);
+    setElapsedLapTime(0);
+
     setLapList([])
   };
 
   const lapStopwatch = () => {
-    setLapList(prevList => [...prevList, formatTime(elapsedTime)]);
+    setLapList(prevList => [...prevList, formatTime(elapsedLapTime)]);
+    if (lapIntervalRef.current !== null) {
+      clearInterval(lapIntervalRef.current);
+      lapIntervalRef.current = null;
+    }
+    setStartLapTime(0);
+    setElapsedLapTime(0);
+
+    const lapNow = Date.now();
+    setStartLapTime(lapNow);
+    if (lapIntervalRef.current !== null) {
+      clearInterval(lapIntervalRef.current);
+    }
+    lapIntervalRef.current = setInterval(() => {
+      setElapsedLapTime(Date.now() - lapNow);
+    }, 100);
   }
 
   useEffect(() => {
@@ -53,12 +95,19 @@ export default function StopWatch() {
       if (Math.abs(deviation) > 100) { // Adjust if deviation is more than 100 milliseconds
         setElapsedTime(actualElapsedTime);
       }
+
+      const actualElapsedLapTime = Date.now() - startLapTime;
+      const lapDeviation = actualElapsedLapTime - elapsedLapTime;
+      // Adjust the stopwatch time if there's a significant deviation
+      if (Math.abs(lapDeviation) > 100) { // Adjust if deviation is more than 100 milliseconds
+        setElapsedLapTime(actualElapsedLapTime);
+      }
     }, 60000); // Calibration interval set to 1 minute
 
     return () => {
       clearInterval(calibrationInterval);
     };
-  }, [elapsedTime, startTime]);
+  }, [elapsedTime, elapsedLapTime, startTime, startLapTime]);
 
   const formatTime = (timeInMillis: number) => {
     const minutes = Math.floor((timeInMillis / (1000 * 60)) % 60);
