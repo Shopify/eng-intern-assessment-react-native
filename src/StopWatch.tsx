a79d744ebf7e939fb08ab981b706e71918c8cdc6
oltimaloku <oltimaloku555@gmail.com>
2024-01-27 16:02:18 -0800

    added initial ui (start, reset, and timer) for stopwatch

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,43 @@
-import { View } from 'react-native';
+import {
+  Button,
+  Dimensions,
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import StopWatchButton from "./StopWatchButton";
 
-export default function StopWatch() {
+const screenWidth = Dimensions.get("window").width;
+
+const StopWatch = () => {
   return (
-    <View >
+    <View>
+      <Text style={styles.timer}>00:00.00</Text>
+      <View style={styles.buttonRow}>
+        <StopWatchButton title="Reset" onPress={() => {}}></StopWatchButton>
+        <StopWatchButton title="Start" onPress={() => {}}></StopWatchButton>
+      </View>
     </View>
   );
-}
+};
+
+export default StopWatch;
+
+const styles = StyleSheet.create({
+  timer: {
+    fontSize: 80,
+    fontWeight: "normal",
+    textAlign: "center",
+  },
+
+  buttonRow: {
+    marginTop: 20,
+    marginHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: screenWidth * 0.9,
+    padding: 10,
+  },
+});
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,8 +1,31 @@
-import { View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function StopWatchButton() {
-  return (
-    <View >
-    </View>
-  );
+interface StopWatchButtonProps {
+  title: string;
+  onPress: () => void;
 }
+
+const StopWatchButton = ({ title, onPress }: StopWatchButtonProps) => {
+  return (
+    <TouchableOpacity style={styles.button}>
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default StopWatchButton;
+
+const styles = StyleSheet.create({
+  button: {
+    width: 90,
+    height: 90,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 15,
+    borderRadius: 50,
+    marginTop: 10,
+  },
+});
