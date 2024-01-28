13848ab6a87d06db281cdc2f1c00d48ad4e36ff6
oltimaloku <oltimaloku555@gmail.com>
2024-01-27 18:35:43 -0800

    Removed unused import

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 interface StopWatchButtonProps {
   title: string;
