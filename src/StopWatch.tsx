ac314d98bd10628bf6127c7cda04f7a64c1eac15
Hanna Zhou <zhou.hanyi@northeastern.edu>
2024-01-27 18:07:07 -0800

    modify StopWatch Component, add useStates

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,20 @@
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import StopWatchButton from './StopWatchButton';
 
-export default function StopWatch() {
-  return (
-    <View >
-    </View>
-  );
+const StopWatch: React.FC = () => {
+  const [issRunning, setIsRunning] = useState(false);
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [laps, setLaps] = useState<number[]>([]);
+
+
+  return (<View style={styles.container}>
+
+    
+  </View>);
 }
+
+const styles = StyleSheet.create({
+  container: {},
+});
