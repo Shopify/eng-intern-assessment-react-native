fa8f672486f4524dfb9f9c729d009880e3586452
bryanjangeesingh <bryanjangeesingh@gmail.com>
2024-01-28 04:05:29 -0500

    wip: finished button definitions

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,8 +1,37 @@
-import { View } from 'react-native';
+import React from "react";
+import { View, Button, StyleSheet } from "react-native";
 
-export default function StopWatchButton() {
+// This file handles buttons and nothing else. Modularize code by passing in props to the renderer.
+
+type Props = {
+  onStartStop: () => void;
+  onLap: () => void;
+  onReset: () => void;
+  isRunning: boolean;
+};
+
+export default function StopwatchButton({
+  onStartStop,
+  onLap,
+  onReset,
+  isRunning,
+}: Props) {
+  // May need to change the texts of these based on what is written in the tests file
   return (
-    <View >
+    <View style={styles.container}>
+      <Button onPress={onStartStop} title={isRunning ? "Stop" : "Start"} />
+      <Button onPress={onLap} title="Lap" disabled={!isRunning} />
+      <Button onPress={onReset} title="Reset" />
     </View>
   );
 }
+
+// Define a stylesheet here
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    padding: 20,
+  },
+});
