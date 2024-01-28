66de44b81cc9846c05dd8a06c882a5c37d349a84
Yazan Armoush <yazan.armoush1@gmail.com>
2024-01-28 05:57:01 -0500

    Made the Buttons Look Better

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 interface Props {
   startStopwatch: () => void;
@@ -7,21 +7,52 @@ interface Props {
   lapStopwatch: () => void;
 }
 
-export default function StopWatchButton({startStopwatch, pauseStopwatch, resetStopwatch, lapStopwatch}: Props) {
+export default function StopWatchButton({ startStopwatch, pauseStopwatch, resetStopwatch, lapStopwatch }: Props) {
   return (
-    <View >
-      <TouchableOpacity onPress={startStopwatch}>
-        <Text >Start</Text>
+    <View style={styles.buttonContainer}>
+      <TouchableOpacity style={[styles.button, styles.startButton]} onPress={startStopwatch}>
+        <Text style={styles.buttonText}>Start</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={pauseStopwatch}>
-        <Text >Pause</Text>
+      <TouchableOpacity style={[styles.button, styles.pauseButton]} onPress={pauseStopwatch}>
+        <Text style={styles.buttonText}>Pause</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={resetStopwatch}>
-        <Text >Reset</Text>
+      <TouchableOpacity style={[styles.button, styles.resetButton]} onPress={resetStopwatch}>
+        <Text style={styles.buttonText}>Reset</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={lapStopwatch}>
-        <Text >Lap</Text>
+      <TouchableOpacity style={[styles.button, styles.lapButton]} onPress={lapStopwatch}>
+        <Text style={styles.buttonText}>Lap</Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 50,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    minWidth: 80,
+    height: 80,
+    marginHorizontal: 10,
+    borderRadius: 40,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  startButton: {
+    backgroundColor: '#4CAF50',
+  },
+  pauseButton: {
+    backgroundColor: '#FFC107',
+  },
+  resetButton: {
+    backgroundColor: '#F44336',
+  },
+  lapButton: {
+    backgroundColor: '#2196F3',
+  },
+});
