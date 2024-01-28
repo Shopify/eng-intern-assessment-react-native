2cadb17ab005591557a31373ab54af501abd8ae7
Yazan Armoush <yazan.armoush1@gmail.com>
2024-01-28 04:45:50 -0500

    Implemented Starting the Watch

--- App.tsx
+++ App.tsx
@@ -2,12 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StopWatch from './src/StopWatch';
 import StopWatchButton from './src/StopWatchButton';
+import { useEffect, useRef, useState } from 'react';
 
 export default function App() {
 
-  const startStopwatch = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [startTime, setStartTime] = useState(0);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalRef = useRef<number | null>(null);
 
-  }
+  const startStopwatch = () => {
+    const now = Date.now() - elapsedTime;
+    setStartTime(now);
+    setIsRunning(true);
+    if (intervalRef.current !== null) {
+      clearInterval(intervalRef.current);
+    }
+    intervalRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - now);
+    }, 10);
+  };
 
   const pauseStopwatch = () => {
 
@@ -21,10 +35,38 @@ export default function App() {
 
   }
 
+  useEffect(() => {
+    // Perform calibration - I do this to make sure the time is accurate
+    const calibrationInterval = setInterval(() => {
+      const actualElapsedTime = Date.now() - startTime;
+      const deviation = actualElapsedTime - elapsedTime;
+      // Adjust the stopwatch time if there's a significant deviation
+      if (Math.abs(deviation) > 100) { // Adjust if deviation is more than 100 milliseconds
+        setElapsedTime(actualElapsedTime);
+      }
+    }, 60000); // Calibration interval set to 1 minute
+
+    return () => {
+      clearInterval(calibrationInterval);
+    };
+  }, [elapsedTime, startTime]);
+
+  const formatTime = (timeInMillis: number) => {
+    const minutes = Math.floor((timeInMillis / (1000 * 60)) % 60);
+    const seconds = Math.floor((timeInMillis / 1000) % 60);
+    const milliseconds = Math.floor((timeInMillis % 1000) / 10);
+    return (
+      `${minutes < 10 ? '0' + minutes : minutes}:` +
+      `${seconds < 10 ? '0' + seconds : seconds}:` +
+      `${milliseconds < 10 ? '0' + milliseconds : milliseconds}`
+    );
+  };
+
+
   return (
     <View style={styles.container}>
       <StopWatch
-        time={'00:00:00'}
+        time={formatTime(elapsedTime)}
       />
       <StopWatchButton
         startStopwatch={startStopwatch}
