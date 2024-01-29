0bb98e060a1e2222113f9a7521db0171a12a2805
Eric Ryu <ryu.eric202@gmail.com>
2024-01-28 22:10:30 -0500

    Testing Update

--- tests/Stopwatch.test.js
+++ tests/Stopwatch.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+import { render, fireEvent, act, findAllByTestId } from '@testing-library/react-native';
 import Stopwatch from '../src/Stopwatch';
 
 describe('Stopwatch', () => {
@@ -7,39 +7,46 @@ describe('Stopwatch', () => {
     const { getByText, queryByTestId } = render(<Stopwatch />);
     
     expect(getByText('00:00:00.00')).toBeTruthy();
-    expect(queryByTestId('lap-list')).toBeNull();
+    expect(queryByTestId('lapTime')).toBeNull();
   });
 
   test('starts and stops the stopwatch', () => {
     const { getByText, queryByText } = render(<Stopwatch />);
     
     fireEvent.press(getByText('Start'));
-    expect(queryByText(/(\d{2}):(\d{2}):(\d{2})\.(\d{2})/)).toBeTruthy();
+    expect(queryByText(/(\d{2}:){2}\d{2}/)).toBeTruthy();
 
     fireEvent.press(getByText('Stop'));
-    expect(queryByText(/(\d{2}):(\d{2}):(\d{2})\.(\d{2})/)).toBeNull();
+    expect(queryByText(/(\d{2}:){2}\d{2}/)).toBeTruthy();
   });
 
-  test('pauses and resumes the stopwatch', () => {
+  test('pauses and resumes the stopwatch', async () => { //changed button labels to Start/Stop as they serve the same function as pause/resume
     const { getByText } = render(<Stopwatch />);
     
-    fireEvent.press(getByText('Start'));
-    fireEvent.press(getByText('Stop'));
-    const pausedTime = getByText(/(\d{2}):(\d{2}):(\d{2})\.(\d{2})/).textContent;
+    await act(async()=>{
 
-    fireEvent.press(getByText('Start'));
-    expect(getByText(/(\d{2}):(\d{2}):(\d{2})\.(\d{2})/).textContent).not.toBe(pausedTime);
+      fireEvent.press(getByText('Start'));
+      await new Promise((resolve) =>setTimeout(resolve, 100))
+      fireEvent.press(getByText('Stop'));
+
+      const pausedTime = await getByText(/(\d{2}:){2}\d{2}/).children.join;
+  
+      fireEvent.press(getByText('Start'));
+
+      await new Promise((resolve) =>setTimeout(resolve, 100))
+
+      expect( await getByText(/(\d{2}:){2}\d{2}/).textContent).not.toBe(pausedTime);
+    })
   });
-
   test('records and displays lap times', () => {
     const { getByText, getByTestId } = render(<Stopwatch />);
     
     fireEvent.press(getByText('Start'));
     fireEvent.press(getByText('Lap'));
-    expect(getByTestId('lapTime')).toContainElement(getByText(/(\d{2}):(\d{2}):(\d{2})\.(\d{2})/));
+    expect(getByTestId('lap-list')).toContainElement(getByText(/(\d{2}:){2}\d{2}/));
 
     fireEvent.press(getByText('Lap'));
-    expect(getByTestId('lapTime').children.length).toBe(2);
+    expect(getByTestId('lap-list').children.length).toBe(2);
   });
 
   test('resets the stopwatch', () => {
