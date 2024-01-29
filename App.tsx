f5d8f3c6933ebd2b6f2e41d3faea32b6b4f0b031
Tom Miller <tommiller@tom.local>
2024-01-29 17:28:16 -0500

    Put stopwatch component in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import StopWatch from './src/StopWatch'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StopWatch />
     </View>
   );
 }
