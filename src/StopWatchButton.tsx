cff975f343c65132cf2c3354bf3dbaf83276f885
Tom Miller <tommiller@tom.local>
2024-01-29 14:42:16 -0500

    Created simple stopwatch button with new hook

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,8 +1,22 @@
-import { View } from 'react-native';
 
-export default function StopWatchButton() {
+import React from 'react';
+import { Button, View } from 'react-native';
+
+export default function StopwatchButton({ onResume, onReset, onLap, onPause, isPaused }: { 
+  onReset: () => void; 
+  onLap: () => void; 
+  onPause: () => void;
+  onResume: () => void;
+  isPaused: boolean;
+}) {
+
   return (
-    <View >
+    <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+      <Button title="Start" onPress={onResume} disabled={!isPaused} />
+      <Button title="Stop" onPress={onPause} disabled={isPaused} />
+      <Button title="Reset" onPress={onReset} />
+      <Button title="Lap" onPress={onLap}  />
     </View>
   );
 }
+
