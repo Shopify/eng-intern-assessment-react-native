3baf267fabc179e46e8e8492c8408cead39a621c
dhruvpatt <84606623+dhruvpatt@users.noreply.github.com>
2024-01-28 22:58:34 -0500

    cleaned up formatTime function and added docstring

--- src/util/FormatTime.tsx
+++ src/util/FormatTime.tsx
@@ -1,19 +1,10 @@
 
-// export default function formatTime(time: number): string {
-//     const hours = Math.floor(time / 3600000);
-//     const minutes = Math.floor((time % 3600000) / 60000);
-//     const seconds = ((time % 60000) / 1000).toFixed(0);
-//
-//     const formattedHours = hours.toString().padStart(2, '0');
-//     const formattedMinutes = minutes.toString().padStart(2, '0');
-//     const formattedSeconds = seconds.toString().padStart(2, '0');
-//
-//     return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
-//
-//
-// };
-//
-
+/**
+ * Formats the given time into a string representation of minutes, seconds, and milliseconds.
+ *
+ * @param {number} time - The time to be formatted in milliseconds.
+ * @return {string} The formatted time string in the format "mm:ss:mm".
+ */
 export default function formatTime(time: number): string {
     // Calculate minutes
     const minutes = Math.floor(time / 60000);
