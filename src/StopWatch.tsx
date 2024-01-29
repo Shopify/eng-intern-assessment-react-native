bcccc9f87c2552da92b3e7202b96d3ed310af89f
Joseph Latina <latinaj2@mymacewan.ca>
2024-01-28 20:33:11 -0700

    added image background and buttons and handled button visuals from play state switching

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,60 @@
-import { View } from 'react-native';
+import { View, StyleSheet, ImageBackground, Text, Image } from 'react-native';
+import StopWatchButton from './StopWatchButton';
+import { useState } from 'react';
+
 
 export default function StopWatch() {
+  // initialize state variables
+  const [play, setPlay] = useState(false);
+  const [playCount, setPlayCount] = useState(0);
+
+  // handler function for play state
+  const handlePlay = () => {
+    setPlay((prevPlay) => !prevPlay);
+    setPlayCount((prevCount) => prevCount + 1);
+  }
+
   return (
-    <View >
-    </View>
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/mobile-background.png")}
+    >
+      <View >
+        <Text></Text>
+        <View style={styles.buttonContainer}>
+          <StopWatchButton 
+            imageName="reset"
+            title="Reset"
+            onPress={() => console.log('Stop Button')}
+          />
+          <StopWatchButton 
+            imageName="play"
+            title="Start"
+            play={play}
+            playCount={playCount}
+            onPress={handlePlay}
+          />
+          <StopWatchButton 
+            imageName="lap"
+            title="Lap"
+            onPress={() => console.log('Stop Button')}
+          />
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+      flex: 1,
+      justifyContent: "flex-end",
+      width: '100%',
+  },
+  buttonContainer: {
+    flexDirection: 'row', // Arrange items horizontally
+    justifyContent: 'space-around', // Add space between items
+    paddingHorizontal: 16, // Add some horizontal padding
+    marginBottom: '30%', // Add some marginBottom if needed
+  },
+})
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,8 +1,82 @@
-import { View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Image, Text, TouchableWithoutFeedback } from 'react-native';
 
-export default function StopWatchButton() {
+// interface for the arguments being passed
+interface StopWatchButtonProps {
+  imageName: string;
+  title: string;
+  play?: boolean;
+  playCount?: number;
+  setPlayCount?: (value: number) => void;
+  onPress: () => void;
+}
+
+// container for all of the different icons to be used for this button component
+const imagePaths: { [key: string]: any } = {
+  play: require('../assets/play.png'),
+  pause: require('../assets/pause.png'),
+  reset: require('../assets/reset.png'),
+  lap: require('../assets/lap.png'),
+};
+
+const StopWatchButton: React.FC<StopWatchButtonProps> = ({ imageName, title, play, playCount, onPress }) => {
+
+  // helper function for handling the image
+  const getImagePath = () => {
+    // if playCount was passed in, return either the play or pause icon depending on state
+    if (playCount !== undefined) {
+      if (play) {
+        return imagePaths.pause;
+      } else {
+        return imagePaths.play;
+      }
+    } 
+    // otherwise, just use the imageName parameter passed in
+    else {
+      return imagePaths[imageName];
+    }
+  }
+
+  // helper function for handling the title
+  const getTitle = () => {
+    // if PlayCount was passed in and timer has already started, return either 'Pause' or 'Resume' depending on the play state
+    if (playCount !== undefined && playCount > 0) {
+      if (play) {
+        return 'Pause';
+      } else {
+        return 'Resume';
+      }
+    } 
+    // otherwise, return the title parameter passed in
+    else {
+      return title;
+    }
+  }
+
   return (
-    <View >
-    </View>
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        <Image style={styles.image} source={getImagePath()}/>
+        <Text>{getTitle()}</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
+
+// Style for this button component
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100, // Set a fixed width for the circular container
+    height: 100, // Set a fixed height for the circular container
+    borderRadius: 50, // Make the container circular
+    backgroundColor: '#ffbc14'
+  },
+  image: {
+    width: 50, // Set the width of the image
+    height: 50, // Set the height of the image
+  }
+})
+
+export default StopWatchButton;
