3180142181421120f4ffb6d5c5a8c903b28a394b
WilsonMungai <wilsonmuguthi@gmail.com>
2024-01-29 00:15:00 -0500

    added StopWatchButton component

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,8 +1,40 @@
-import { View } from 'react-native';
+import React, { FC } from 'react';
+import { TouchableOpacity, Text, StyleSheet, StyleProp, ViewStyle } from 'react-native';
 
-export default function StopWatchButton() {
-  return (
-    <View >
-    </View>
-  );
+interface StopWatchButtonProps {
+  title: string;
+  onPress: () => void;
+  running: boolean;
+  disabled: boolean;
 }
+
+const StopWatchButton: FC<StopWatchButtonProps> = ({ title, onPress, running, disabled }) => {
+
+  const buttonStyle: StyleProp<ViewStyle> = {
+    backgroundColor: running ? "red" : "green",
+  };
+
+  return (
+    <TouchableOpacity style={[styles.button, buttonStyle]} onPress={onPress} disabled={disabled}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 8,
+    height: 50,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default StopWatchButton;
