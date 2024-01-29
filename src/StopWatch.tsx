ee1039064e4f0aff7c83e3f7c52b1d660ebed835
csc530 <77406318+csc530@users.noreply.github.com>
2024-01-28 19:51:32 -0500

    :sparkles: feat(stopwatch): add pause and resume button
    change how time is tracked in the stopwatch and displayed
    test for pause and resume still fail since it wants a difference between the time it pause at and immidiately when it resumes without delay, so yeah

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,54 +1,81 @@
-import {Text} from 'react-native';
+import {Text} from "react-native";
 import interval, {Interval} from "./Interval";
 import {useState} from "react";
 import StopWatchButton from "./StopWatchButton";
 
-function elapsedTimeString(elapsedTime: interval | null) {
-    if (elapsedTime === null)
+function elapsedTimeString(from: Date | null, to: Date | null = new Date()): string {
+    if(to === null && from === null || to === null)
         return "--:--:--";
-    const padTimeString = (n: number) => n < 10 ? `0${n}` : `${n}`
+    if(from === null)
+        return "00:00:00";
 
-    const {hours, minutes, seconds} = elapsedTime;
+    const padTimeString = (n: number) => n < 10 ? `0${n}` : `${n}`;
+    const {hours, minutes, seconds} = Interval(to.valueOf() - from.valueOf());
 
     return `${padTimeString(hours)}:${padTimeString(minutes)}:${padTimeString(seconds)}`;
 }
 
-// ! but for SOME (react 🙄) reason, this doesn't work inline
 const incrementIntervalBy1Second = (interval: interval | null): interval => {
-    if (interval === null)
+    if(interval === null)
         interval = Interval(0);
-    return Interval(interval.totalMilliseconds + 1000);
-}
+    return Interval(interval.totalMilliseconds + 1);
+};
 
 export default function StopWatch() {
+    const [clock, setClock] = useState<Date | null>(null);
+    const [toClock, setToClock] = useState<Date | null>(new Date());
     // used to stop setInterval
-    let [stopwatchId, setStopwatchId] = useState(NaN);
-    // elapsed time in milliseconds
-    let [elapsedTime, setElapsedTime] = useState<interval | null>(Interval(0));
+    const [stopwatchId, setStopwatchId] = useState(NaN);
+    const [elapsedTime, setElapsedTime] = useState<string>("--:--:--");
 
     const startClock = () => {
-        setElapsedTime(Interval(0));
-
-        setStopwatchId(
-            setInterval(() => {
-                setElapsedTime(incrementIntervalBy1Second);
-            }, 1000)
-        );
-    }
+        setClock(new Date());
+        resumeClock();
+    };
 
     const stopClock = () => {
+        pauseClock();
+        setClock(null);
+        setToClock(null);
+    };
+
+    const pauseClock = () => {
         clearInterval(stopwatchId);
         setStopwatchId(NaN);
-        setElapsedTime(null)
-    }
+    };
+
+    const resumeClock = () => {
+        if(toClock === null)
+            setToClock(new Date()); //should never happen; suppress ts warning below
+        else if(clock === null)
+            setClock(new Date());
+        else {
+            // add elapsed time to start `clock` so it continues where it left off
+            // i.e., fast-forward in time to account for time spent paused
+            const elapsedMilliseconds = new Date().valueOf() - toClock.valueOf(); //toClock is not null @->resumeClock
+            setClock(new Date(clock.valueOf() + elapsedMilliseconds));
+            setToClock(new Date());
+        }
+        setStopwatchId(
+            setInterval(() => {
+                setToClock(new Date());
+                setElapsedTime(elapsedTimeString(clock, new Date()));
+            }, 1000)
+        );
+    };
 
 
     return (
         <>
-            <Text>{elapsedTimeString(elapsedTime)}</Text>
+            <Text>{elapsedTimeString(clock, toClock)}</Text>
             <StopWatchButton
                 text={stopwatchId ? "Stop" : "Start"}
                 onClick={stopwatchId ? stopClock : startClock}
+            />
+
+            <StopWatchButton
+                text={stopwatchId ? "Pause" : "Resume"}
+                onClick={stopwatchId ? pauseClock : resumeClock}
             />
         </>
     );
