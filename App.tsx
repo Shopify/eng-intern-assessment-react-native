f02b1a014a7b444d266d1336cbdd20b563a21503
sanya <sanya.chawla@mail.utoronto.ca>
2024-01-29 20:23:29 -0500

    Added all the components and states to the App

--- App.tsx
+++ App.tsx
@@ -1,11 +1,63 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { StopWatch } from './src/StopWatch';
+import { StopWatchButton } from './src/StopWatchButton';
+import { LapList } from './src/LapList'; // Import the LapList component
+
+const CURRENT_TIME = 0;
 
 export default function App() {
+  const [timeCount, setTimeCount] = useState<number>(CURRENT_TIME);
+  const [timeInterval, setTimeInterval] = useState<NodeJS.Timeout | null>(null);
+  const [isStopWatchRunning, setIsStopWatchRunning] = useState<boolean>(false);
+  const [laps, setLaps] = useState<number[]>([]);
+
+  const startTime = () => {
+    setIsStopWatchRunning(true);
+
+    const id = setInterval(() => setTimeCount((prev) => prev + 10), 10);
+    setTimeInterval(id);
+  };
+
+  const stopTime = () => {
+    if (timeInterval !== null) {
+      clearInterval(timeInterval);
+      setTimeInterval(null);
+    }
+    setIsStopWatchRunning(false);
+  };
+
+  const resetTime = () => {
+    setTimeCount(0);
+    setLaps([]); // Clear laps when resetting
+  };
+
+  const addLap = () => {
+    setLaps((prevLaps) => [...prevLaps, timeCount]);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timeInterval !== null) {
+        clearInterval(timeInterval);
+        setTimeInterval(null);
+      }
+    };
+  }, [timeInterval]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <StopWatch stopWatchCount={timeCount} />
+      <StopWatchButton
+        isStopWatchRunning={isStopWatchRunning}
+        startTime={startTime}
+        stopTime={stopTime}
+        resetTime={resetTime}
+        addLap={addLap}
+      />
+      <LapList laps={laps} />
     </View>
   );
 }
