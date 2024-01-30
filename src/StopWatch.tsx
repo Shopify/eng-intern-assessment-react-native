0f99d883719ae54b183cc631161a9df9735016fb
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2024-01-29 23:40:52 -0500

    Create stopwatch component

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,15 @@
-import { View } from 'react-native';
+import React, { useState, useRef, useCallback } from "react";
+import { StyleSheet, SafeAreaView, Text, View, Platform } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import Constants from "expo-constants";
+
+import StopWatchButton from "./StopWatchButton";
 
 export default function StopWatch() {
-  return (
-    <View >
-    </View>
-  );
+  const [time, setTime] = useState<number>(0);
+  const [isRunning, setRunning] = useState<boolean>(false);
+  const [results, setResults] = useState<number[]>([]);
+
+
+  return ;
 }
