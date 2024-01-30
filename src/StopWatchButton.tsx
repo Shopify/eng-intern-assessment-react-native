92287b2a0d4ba60401db0ebdc20825cb4330339f
sanya <sanya.chawla@mail.utoronto.ca>
2024-01-29 21:22:01 -0500

    Final Changes for Buttons

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -2,93 +2,97 @@ import React from 'react';
 import { Pressable, View, StyleSheet, Text } from 'react-native';
 
 // Declares props that will be used by the StopWatch Buttons
-type Props = {
+  type Props = {
 
-  // Keeps track of whether stopwatch is running or not
-  isStopWatchRunning: boolean;
+    // Keeps track of whether stopwatch is running or not
+    isStopWatchRunning: boolean;
+    
+    // Function that stops time
+    stopTime: () => void;
+    
+    // Function that starts time
+    startTime: () => void;
+    
+    // Function that resets the time
+    resetTime: () => void;
+    
+    // Functions that add a lap to lap-list
+    addLap: () => void;
 
-  // Function that stops time
-  stopTime: () => void;
+  };
 
-  // Function that starts time
-  startTime: () => void;
+  export const StopWatchButton: React.FC<Props> = ({
+    isStopWatchRunning,
+    stopTime,
+    startTime,
+    resetTime,
+    addLap,
+    }) => {
 
-  // Function that resets the time
-  resetTime: () => void;
+    // Start/Stop button color that changes depending on state
+    const startStopButtonColor = isStopWatchRunning ? '#e37878' : '#57ba8d';
 
-  // Functions that adds a lap to lap-list
-  addLap: () => void;
+    return (
+      <View style={styles.buttonsContainer}>
+        
+        {/* Add lap / Reset Time Button */}
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? 'lightgray' : '#383838',
+              marginRight: 50,
+            },
+            styles.pressableButton,
+          ]}
+          onPress={isStopWatchRunning ? addLap : resetTime}
+        >
+          <Text style={styles.buttonText}>{isStopWatchRunning ? 'Lap' : 'Reset'}</Text>
+        </Pressable>
 
-};
-
-export const StopWatchButton: React.FC<Props> = ({
-
-  isStopWatchRunning,
-  stopTime,
-  startTime,
-  resetTime,
-  addLap,
-
-}) => {
-
-  // Start/Stop button color that changes depending on state
-  const startStopButtonColor = isStopWatchRunning ? '#e37878' : '#57ba8d';
-
-  return (
-    <View style={styles.buttonsContainer}>
+        {/* Start/Pause Button */}
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? 'lightgray' : startStopButtonColor,
+            },
+            styles.pressableButton,
+          ]}
+          onPress={isStopWatchRunning ? stopTime : startTime}
+        >
+          <Text style={styles.buttonText}>{isStopWatchRunning ? 'Stop' : 'Start'}</Text>
+        </Pressable>
+      </View>
       
-      {/* Add lap / Reset Time Button */}
-      <Pressable
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? 'lightgray' : '#383838',
-            marginRight: 50,
-          },
-          styles.pressableButton,
-        ]}
-        onPress={isStopWatchRunning ? addLap : resetTime}
-      >
-        <Text style={{ color: 'white' }}>{isStopWatchRunning ? 'Lap' : 'Reset'}</Text>
-      </Pressable>
+    );
+  };
 
 
-      {/* Start/Pause Button */}
-      <Pressable
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? 'lightgray' : startStopButtonColor,
-          },
-          styles.pressableButton,
-        ]}
-        onPress={isStopWatchRunning ? stopTime : startTime}
-      >
-        <Text style={{ color: 'white' }}>{isStopWatchRunning ? 'Stop' : 'Start'}</Text>
-      </Pressable>
-    </View>
-
-  );
-
-};
-
-
-// StopWatch Buttons Styles
-const styles = StyleSheet.create({
-
-  // Displays Buttons in a row
-  buttonsContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 10
-  },
-
-  // Style for all buttons
-  pressableButton: {
-    elevation: 5,
-    borderRadius: 50,
-    width: 75,
-    height: 75,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-});
+  // StopWatch Buttons Styles
+  const styles = StyleSheet.create({
+   
+    // Displays Buttons in a row
+    buttonsContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: 10,
+    },
+    
+    // Style for all buttons
+    pressableButton: {
+      elevation: 5,
+      borderRadius: 50,
+      width: 75,
+      height: 75,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    
+    // Text style for buttons
+    buttonText: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: '400',
+    },
+  
+    
+  });
