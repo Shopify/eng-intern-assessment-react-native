f4f6e90a2932cf5a387716846b572a4aa684b18b
sanya <sanya.chawla@mail.utoronto.ca>
2024-01-29 20:16:04 -0500

    Updated LapList.tsx and the format of the time

---
+++ src/LapList.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+// Declares props that will be used by the StopWatch Buttons
+type Props = {
+
+    // List that keeps track of the laps
+    laps: number[];
+};
+  
+  export const LapList: React.FC<Props> = ({ laps }) => {
+    const renderItem = ({ item, index }: { item: number; index: number }) => (
+
+    // Display the times of the laps 
+      <View style={styles.lapItem}>
+        <Text>{`Lap ${index + 1}: ${formatTime(item)}`}</Text>
+      </View>
+
+    );
+  
+    // Format the time of the laps in HH:MM:SS format
+    const formatTime = (time: number): string => {
+        const hours = Math.floor(time / (1000 * 60 * 60)).toString().padStart(2, '0');
+        const minutes = Math.floor(time / (1000 * 60)).toString().padStart(2, '0');
+        const seconds = Math.floor((time % (1000 * 60)) / 1000).toString().padStart(2, '0');
+        const milliseconds = (time % 1000).toString().padStart(3, '0').slice(0, 2);
+        return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    };
+  
+    return (
+    
+    
+        // List of the Laps using FlatList
+        <View style={styles.lapListContainer}>
+            <FlatList
+                data={laps}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => index.toString()}
+                style={styles.lapList}
+            />
+        </View>
+    );
+
+  };
+  
+  // LapList Styles
+  const styles = StyleSheet.create({
+    lapListContainer: {
+      marginTop: 10,
+      flex: 1,
+      width: '100%',
+  
+    },
+    
+    lapList: {
+      width: '100%',
+    },
+
+    lapItem: {
+      padding: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: '#ccc',
+    },
+
+});
