4c2a34eed2290fb87a7e317367a6ba952f4c411e
Abilaash <abilaash2001@gmail.com>
2024-01-29 23:07:33 -0500

    assessment code updated for stopwatch and button features

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { StopWatch } from './src/StopWatch';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <StopWatch />
       <StatusBar style="auto" />
     </View>
   );
